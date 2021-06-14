e7309a608ecc399522c69ec266e095cdbf343805
Kbefus <kmbefus@uark.edu>
2021-06-14 17:56:52 -0500

    Streamlines to loading shps and raster basemap
    I'm not sure you want all these changes, but I added what I think are streamlined ways for loading and plotting shapefiles (all need the same attributes). Adds raster as image layer (toggle-able layer, with some transparency over basemap). I don't think I broke anything. Adding a colorbar for the raster water table depth is going to be a task - probably easier to save a legend as a png and then plot it somewhere on the map.

--- streamlit/GroMoPo_app.py
+++ streamlit/GroMoPo_app.py
@@ -1,6 +1,8 @@
 # IMPORTS
 import streamlit as st
 import geopandas as gpd
+import rasterio as rio
+import numpy as np
 import folium
 from folium.features import GeoJsonPopup, GeoJsonTooltip
 from folium import plugins
@@ -11,6 +13,39 @@ from pathlib import Path
 import platform
 import os
 
+#%% Supporting functions - eventually create function library?
+
+# Configure multipage selector — reads .md files
+def read_markdown_file(markdown_file):
+    return Path(markdown_file).read_text()
+
+def load_rast(fname,band=0):
+    with rio.open(fname) as src:
+        img = src.read()[band]
+        # bounds = src.bounds[:]
+        # bounds = [[bounds[1],bounds[0]],[bounds[3],bounds[2]]]
+    return img
+
+def cmap(img,cmap='viridis',nan_val=None,vminmax=[0,100]):
+    import matplotlib.cm as cm # only called once
+    if nan_val is None:
+        # Assume lowest value is nan value to skip in colormap
+        nan_val = img.min()
+    
+    img2 = img.copy()
+    img2[img2==nan_val] = np.nan
+    cmap_obj = cm.get_cmap(cmap)
+    if vminmax[0] is None:
+        norm_data =(img2 - np.nanmin(img2))/(np.nanmax(img2)-np.nanmin(img2))
+    else:
+        norm_data = (img2-vminmax[0])/(vminmax[1]-vminmax[0])
+    cm_out = cmap_obj(norm_data)
+    cm_out[np.isnan(img2),:] = 1
+    return cm_out
+    
+    
+    
+#%%
 # Configure for wide layout
 st.set_page_config(layout="wide")
 
@@ -18,7 +53,7 @@ st.set_page_config(layout="wide")
 st.sidebar.title('Navigation')
 
 selection = st.sidebar.radio("Go to",['Home','Find Models','Submit Model','About'])
-
+# selection='Find Models'
 st.sidebar.title('Contribute')
 
 st.sidebar.info("This an open source project and you are very welcome to **contribute** your awesome comments, questions, resources and groundwater models to the source code")
@@ -33,10 +68,6 @@ else:
     image_path = os.getcwd() + '/streamlit/GroMoPo_logo_V1.png'
 
 st.sidebar.image(image_path, caption=None, width=None, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
-
-# Configure multipage selector — reads .md files
-def read_markdown_file(markdown_file):
-    return Path(markdown_file).read_text()
 
 if selection == 'Home':
     if platform.system() == 'Darwin':
@@ -71,92 +102,123 @@ if selection == 'About':
 
 # Main app page — groundwater model display and search map
 # NOTE: this could be refactored into a separate .py script and import on app start
-
+epsg = 3857# mercator default 
 if selection == 'Find Models':
     st.title('GroMoPo — Groundwater Model Portal')
 
     st.write("Sharing groundwater model data, knowledge and insights more easily through a portal of regional and global numerical groundwater models. The first priority is archiving existing models, but the repository could eventually archive model input and scripts for translating commonly used geospatial datasets into model inputs.")
+    
+    # Load each continent and concatenate - might make more sense to load only one global shp in
+    continents = ['africa','oceania','asia','europe','north_america','south_america']
+    shp_dir = os.path.join('data','shapes')
+    
+    all_gdfs = []
+    for continent in continents:
+        if platform.system() == 'Darwin':
+            shp_fname = os.path.join('..',shp_dir,'{}.shp'.format(continent))
+            # AUS_gdf_polygs = gpd.read_file('../QGIS/shapes/Australia.shp')
+            # NA_gdf_polygs = gpd.read_file('../QGIS/shapes/north_america.shp')
+        else:
+            shp_fname = os.path.join(os.getcwd(),shp_dir,'{}.shp'.format(continent))
+            # AUS_gdf_polygs = gpd.read_file(os.getcwd() + '/QGIS/shapes/Australia.shp')
+            # NA_gdf_polygs = gpd.read_file(os.getcwd() + '/QGIS/shapes/north_america.shp')
+        if os.path.isfile(shp_fname):
+            temp_df=gpd.read_file(shp_fname)
+            if temp_df.crs.to_epsg() != epsg:
+                temp_df.to_crs(epsg=epsg,inplace=True)
+            all_gdfs.append(temp_df)
+    
+    all_gdf = gpd.GeoDataFrame(gpd.pd.concat(all_gdfs)) # one shp to plot, requires consistent attributes
+    if all_gdf.crs.to_epsg() != epsg: # convert crs, if needed
+        all_gdf.to_crs(epsg=epsg,inplace=True)
+    
 
-    if platform.system() == 'Darwin':
-        AUS_gdf_polygs = gpd.read_file('../QGIS/shapes/Australia.shp')
-        NA_gdf_polygs = gpd.read_file('../QGIS/shapes/north_america.shp')
-    else:
-        AUS_gdf_polygs = gpd.read_file(os.getcwd() + '/QGIS/shapes/Australia.shp')
-        NA_gdf_polygs = gpd.read_file(os.getcwd() + '/QGIS/shapes/north_america.shp')
-
-    AUS_gdf_polygs = AUS_gdf_polygs.to_crs(epsg='3857')
-    AUS_gdf_points = AUS_gdf_polygs.copy()
-    AUS_gdf_points["geometry"] = AUS_gdf_points["geometry"].centroid
-    AUS_gdf_points = AUS_gdf_points.to_crs(epsg='4326')
-    AUS_gdf_points['lon'] = AUS_gdf_points.geometry.x
-    AUS_gdf_points['lat'] = AUS_gdf_points.geometry.y
-
-    NA_gdf_polygs = NA_gdf_polygs.to_crs(epsg='3857')
-    NA_gdf_points = NA_gdf_polygs.copy()
-    NA_gdf_points["geometry"] = NA_gdf_points["geometry"].centroid
-    NA_gdf_points = NA_gdf_points.to_crs(epsg='4326')
-    NA_gdf_points['lon'] = NA_gdf_points.geometry.x
-    NA_gdf_points['lat'] = NA_gdf_points.geometry.y
-
-    map = folium.Map(zoom_start=3, crs='EPSG3857')
+    map = folium.Map(zoom_start=3, crs='EPSG{}'.format(epsg),min_zoom=3,max_bounds=True)
 
     folium.TileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', attr='x').add_to(map)
     folium.TileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', attr='x').add_to(map)
+    
+    rast_fname = os.path.join(os.path.dirname(os.path.dirname(shp_fname)),'degraaf_gw_dep.map')
+    img = load_rast(rast_fname) # 36 MB, not sure effect on load time from github
+    cm_out = cmap(img)
+    skip_rows=60
+    cm_out = cm_out[skip_rows:-skip_rows,:,:]
+    
+    rgroup = folium.FeatureGroup(name='Water table depth [de Graaf]').add_to(map)
+    
+    rgroup.add_child(folium.raster_layers.ImageOverlay(cm_out,opacity=0.6,bounds=[[-90,-180],[90,180]],mercator_project=True))#.add_to(map) #
+    
+    
+    
+    marker_cluster = plugins.MarkerCluster(control=False).add_to(map)
 
-    marker_cluster = plugins.MarkerCluster().add_to(map)
+    # popup_AU = GeoJsonPopup(
+    #     fields=["Custodian", "Dev date", "Code"],
+    #     aliases=["Custodian", "Dev date", "Code"],
+    #     localize=True,
+    #     labels=True,
+    #     style="background-color: yellow;",
+    # )
 
-    popup_AU = GeoJsonPopup(
-        fields=["Custodian", "Dev date", "Code"],
-        aliases=["Custodian", "Dev date", "Code"],
-        localize=True,
-        labels=True,
-        style="background-color: yellow;",
-    )
-
-    tooltip_AU = GeoJsonTooltip(
-        fields=["Custodian", "Dev date", "Code"],
-        aliases=["Custodian", "Dev date", "Code"],
-        localize=True,
-        sticky=False,
-        labels=True,
-        style="""
-            background-color: #F0EFEF;
-            border: 2px solid black;
-            border-radius: 3px;
-            box-shadow: 3px;
-        """,
-        max_width=800,
-    )
+    # tooltip_AU = GeoJsonTooltip(
+    #     fields=["Custodian", "Dev date", "Code"],
+    #     aliases=["Custodian", "Dev date", "Code"],
+    #     localize=True,
+    #     sticky=False,
+    #     labels=True,
+    #     style="""
+    #         background-color: #F0EFEF;
+    #         border: 2px solid black;
+    #         border-radius: 3px;
+    #         box-shadow: 3px;
+    #     """,
+    #     max_width=800,
+    # )
 
     popup_NA = GeoJsonPopup(
-            fields=["id", "devdate", "name", "url", "contributo", "custodian", "spscale", "purpose", "archive", "coupling", "contribu_1"],
-            aliases=["id", "devdate", "name", "url", "contributo", "custodian", "spscale", "purpose", "archive", "coupling", "contribu_1"],
+            fields=["id", "devdate", "name", "url", "custodian", "spscale", "purpose", "archive", "coupling", "contribu_1"],
+            aliases=["ID", "Date Developed", "Name", "URL", "Custodian", "Spatial Scale", "Purpose", "Archive", "Coupling", "Contributed by"],
             localize=True,
             labels=True,
             style="background-color: yellow;",
         )
 
+    for _, r in all_gdf.to_crs(epsg='4326').iterrows():
+        folium.Marker(location=[r.geometry.centroid.y, r.geometry.centroid.x]).add_to(marker_cluster)
 
-    for _, r in AUS_gdf_points.iterrows():
-        folium.Marker(location=[r['lat'], r['lon']]).add_to(marker_cluster)
-
-    for _, r in NA_gdf_points.iterrows():
-        folium.Marker(location=[r['lat'], r['lon']]).add_to(marker_cluster)
-
-    g = folium.GeoJson(AUS_gdf_polygs, popup=popup_AU, tooltip=tooltip_AU, style_function = lambda feature: {
-            'fillColor': 'grey',
-            'weight': 1,
-            'fillOpacity': 0.7,
-    }).add_to(map)
-
-    h = folium.GeoJson(NA_gdf_polygs, popup=popup_NA, style_function = lambda feature: {
+    h = folium.GeoJson(all_gdf,name='Groundwater models', popup=popup_NA, style_function = lambda feature: {
                 'fillColor': 'grey',
                 'weight': 1,
                 'fillOpacity': 0.7,
     }).add_to(map)
 
+    # for _, r in AUS_gdf_points.iterrows():
+    #     folium.Marker(location=[r['lat'], r['lon']]).add_to(marker_cluster)
+
+    # for _, r in NA_gdf_points.iterrows():
+    #     folium.Marker(location=[r['lat'], r['lon']]).add_to(marker_cluster)
+
+    # g = folium.GeoJson(AUS_gdf_polygs, popup=popup_AU, tooltip=tooltip_AU, style_function = lambda feature: {
+    #         'fillColor': 'grey',
+    #         'weight': 1,
+    #         'fillOpacity': 0.7,
+    # }).add_to(map)
+
+    # h = folium.GeoJson(NA_gdf_polygs, popup=popup_NA, style_function = lambda feature: {
+    #             'fillColor': 'grey',
+    #             'weight': 1,
+    #             'fillOpacity': 0.7,
+    # }).add_to(map)
+
     map.add_child(folium.LayerControl())
+    # if epsg == 3857:
+    #     map.setMaxBounds([[-20026376.39, -20048966.10],
+    #                       [20026376.39, 20048966.10]]) # if EPSG=3857 # Pseudo-Mercator
+    # elif epsg == 4326:
+    #     map.setMaxBounds([[-180,-85.06],[180,85.06]]) # if EPSG=4326 # WGS84
 
     Fullscreen().add_to(map)
 
     folium_static(map, height=700, width=1400)
+    
+    # map.save('test.html')
