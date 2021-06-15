7a704e5bcbf6f272262b202c17d6106b47159b67
Kbefus <kmbefus@uark.edu>
2021-06-14 20:49:19 -0500

    Maybe a netcdf of all rasters will be better

--- streamlit/GroMoPo_app.py
+++ streamlit/GroMoPo_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import geopandas as gpd
 import matplotlib.cm as cm
-from osgeo import gdal
+import netCDF4
 import numpy as np
 import folium
 from folium.features import GeoJsonPopup, GeoJsonTooltip
@@ -145,14 +145,14 @@ if selection == 'Find Models':
     
     rast_fname = os.path.join(os.path.dirname(os.path.dirname(shp_fname)),'degraaf_gw_dep.map')
     # img = load_rast(rast_fname) # 36 MB, not sure effect on load time from github
-    img = gdal.Open(rast_fname).ReadAsArray()
+    # img = gdal.Open(rast_fname).ReadAsArray()
         
-    cm_out = cmap(img)
-    skip_rows=60
-    cm_out = cm_out[skip_rows:-skip_rows,:,:]
-    rgroup = folium.FeatureGroup(name='Water table depth [de Graaf] (Yellow = >100 m | Blue = <=0 m)').add_to(map)
+    # cm_out = cmap(img)
+    # skip_rows=60
+    # cm_out = cm_out[skip_rows:-skip_rows,:,:]
+    # rgroup = folium.FeatureGroup(name='Water table depth [de Graaf] (Yellow = >100 m | Blue = <=0 m)').add_to(map)
     
-    rgroup.add_child(folium.raster_layers.ImageOverlay(cm_out,opacity=0.6,bounds=[[-90,-180],[90,180]],mercator_project=True))#.add_to(map) #
+    # rgroup.add_child(folium.raster_layers.ImageOverlay(cm_out,opacity=0.6,bounds=[[-90,-180],[90,180]],mercator_project=True))#.add_to(map) #
     
     
     
