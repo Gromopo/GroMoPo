31627eeed27c33ab380c0e9dd5df33f3c106d648
kbefus <kbefus@uark.edu>
2022-01-13 12:43:48 -0600

    shots in the dark

--- streamlit/pages/home.py
+++ streamlit/pages/home.py
@@ -3,7 +3,8 @@ from utils import helpers as hp
 
 
 def app():
-	st.markdown(hp.read_markdown_file("/pages/view/home_page.md"), unsafe_allow_html=True)
-	st.image("/pages/img/GroMoPohomebanner.png", caption=None, width=None, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
+	st.markdown(hp.read_markdown_file("./pages/view/home_page.md"), unsafe_allow_html=True)
+	st.image("./pages/img/GroMoPohomebanner.png", caption=None, width=None, use_column_width=None,
+          clamp=False, channels='RGB', output_format='auto')
 
 
