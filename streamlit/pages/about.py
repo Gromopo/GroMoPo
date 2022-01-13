6428c7aacd7a1daf03fbc02b8f0038b36773d691
kbefus <kbefus@uark.edu>
2022-01-13 13:22:52 -0600

    path correction

--- streamlit/pages/about.py
+++ streamlit/pages/about.py
@@ -3,6 +3,6 @@ from utils import helpers as hp
 from pathlib import Path
 
 def app():
-    main_path = Path(".")
+    main_path = Path("streamlit")
     markdown = hp.read_markdown_file(str(main_path.joinpath('pages','view','about_page.md')))
     st.markdown(markdown, unsafe_allow_html=True)
--- streamlit/pages/home.py
+++ streamlit/pages/home.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 
 def app():
-    main_path = Path(".")
+    main_path = Path("streamlit")
     st.markdown(hp.read_markdown_file(str(main_path.joinpath('pages','view','home_page.md'))), unsafe_allow_html=True)
     st.image(str(main_path.joinpath('img','GroMoPohomebanner.png')), caption=None,
           width=None, use_column_width=None,clamp=False, channels='RGB', output_format='auto')
