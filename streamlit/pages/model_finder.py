14a6470d51c6343cbaeadf57f9b2b49583ce3cfd
Robert Reinecke <robert.reinecke@uni-potsdam.de>
2022-06-08 11:00:12 +0200

    Added changes from Kevin B.

--- streamlit/pages/model_finder.py
+++ streamlit/pages/model_finder.py
@@ -1,6 +1,7 @@
+from collections import OrderedDict
 import streamlit as st
 import folium
-from folium.features import GeoJsonPopup, GeoJsonTooltip
+from folium.features import GeoJsonPopup#, GeoJsonTooltip
 from folium import plugins
 from folium.plugins import Fullscreen
 from streamlit_folium import folium_static
@@ -9,7 +10,7 @@ from matplotlib.pyplot import imread
 from pathlib import Path
 import platform
 
-from utils import helpers as hp
+#from utils import helpers as hp
 
 # This and all map related commands would be nice to cache, but not easy:
 # https://github.com/randyzwitch/streamlit-folium/issues/4
@@ -27,7 +28,6 @@ def plot_map(shp_df, rasters=None, popup=None,to_epsg='4326'):
     
     marker_cluster = plugins.MarkerCluster(control=False)
     mlayers = []
-    
     # Domains with detailed spatial extents
     for _, r in shp_df.to_crs(epsg=to_epsg).iterrows():
         folium.Marker(location=[r.geometry.centroid.y, r.geometry.centroid.x]).add_to(marker_cluster)
@@ -97,25 +97,126 @@ if platform.system() == 'Windows':
 else:
     main_path = Path(".")
 
-popup = GeoJsonPopup(
-            fields=["id", "devdate", "name", "url", "custodian",
-                    "spscale", "purpose", "archive", "coupling", "contribu_1"],
-            aliases=["ID", "Date Developed", "Name", "URL", "Custodian", "Spatial Scale",
-                     "Purpose", "Archive", "Coupling", "Contributed by"],
-            localize=True,
-            labels=True,
-            style="background-color: yellow;",
-        )
+
+popup_dict = OrderedDict({"id":"ID", "devdate":"Date developed", "name":"Model name",
+              "url":"Publication link(s)", "custodian":"Authors",
+                    "spscale":"Spatial scale", "purpose":"Purpose", "archive":"Model link(s)",
+                    "coupling":"Coupling", "contribu_1":"Contributed by"})
+
+@st.cache  
+def popupHTML(row, popup_dict=popup_dict,col1width=50):
+    '''Create custom HTML for popup.
+
+    Parameters
+    ----------
+    row : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    html : TYPE
+        DESCRIPTION.
+        
+    After: https://towardsdatascience.com/folium-map-how-to-create-a-table-style-pop-up-with-html-code-76903706b88a
+
+    '''
+    html = """<!DOCTYPE html>
+            <html>
+            
+            <table>
+            <tbody>
+            """
+    
+    for key, value in popup_dict.items():
+        # Loop through entries for popup with specific formatting
+        
+        if row[key] is None:
+            second_col_val= 'N/A'
+        else:
+            second_col_val = row[key]
+        
+        if 'link' in value: # format for clickable link
+            html += """<tr>
+                       <td><span style="width: {0}px; color: #293191; overflow-wrap: break-word;"><b>{1}</b></span></td>
+                       <td><span style="overflow-wrap: break-word;>""".format(col1width,value) # Attribute name
+                       
+            # if second_col_val != 'N/A' and second_col_val is not None:
+                
+            links = second_col_val.split('|')
+            links.insert(0,"") # for some reason need a dummy entry, as first entry doesn't show up as link, only as text.
+            
+                
+            # link_html = """ """.join(["""<a href="{0}" target="_blank"> {0}</a><br>""".format(link) for link in links])
+            link_html = """ """
+            for i,link in enumerate(links):
+                if i == 0:
+                    link_html += """<a href="{0}" target="_blank">{0}</a>""".format(link)
+                elif link == 'N/A':
+                    link_html += """{}<br>""".format(link)
+                else:
+                    link_html += """<a href="{0}" target="_blank">{0}</a><br>""".format(link)
+            
+            html += link_html
+            # else:
+            #     html += """{0}<br>""".format(second_col_val)
+                
+            html += """</span></td></tr>"""
+            # html += """</td></td></td></tr>"""
+
+
+        else:
+            html += """<tr>
+                       <td><span style="width: {0}px;color: #000000;overflow-wrap: break-word;"><b>{1}</b></span></td>
+                       <td style="overflow-wrap: break-word;">{2}</td></tr>""".format(col1width,value,second_col_val)
+                       
+    
+    html += """</tbody>
+               </table>                    
+               </html>"""
+    
+    return html
+
+# popup = GeoJsonPopup(
+#             fields=["id", "devdate", "name", "url", "custodian",
+#                     "spscale", "purpose", "archive", "coupling", "contribu_1"],
+#             aliases=["ID", "Date Developed", "Name", "URL", "Custodian", "Spatial Scale",
+#                      "Purpose", "Archive", "Coupling", "Contributed by"],
+#             localize=True,
+#             labels=True,
+#             style="background-color: yellow;overflow-wrap: break-word;",
+#         )
+popup = GeoJsonPopup(labels=False,fields=["popup_html"],
+                     localize=True,style="background-color: blue;overflow-wrap: break-word;")
 
 epsg = 3857
 # Load shapefiles of models
 all_gdf, shp_dir = load_shp(main_path, epsg=epsg) #Path().absolute()
+# Replace None values as "N/A"
+objc = list(all_gdf.select_dtypes(include=['object']).columns.values)
+all_gdf[objc] = all_gdf[objc].replace([None],'N/A')
+
+# Create popup html attribute
+all_gdf['popup_html'] = all_gdf.apply(popupHTML,axis=1)
+
 # print(Path().absolute())
 #Load water table base map
 rast_fname = str(main_path.absolute().joinpath('data', 'degraaf_gw_dep.png'))
 img = read_img(rast_fname)
 rasters_dict = {'degraaf_dep':{'name':'Water table depth [de Graaf] (Yellow = >100 m | Blue = <=0 m)',
                                'data':img}}
+
+# pt_fname = str(main_path.absolute().joinpath('data', 'sprint_11_2021_plot.csv'))
+# ptsq_df = csv2shp(pt_fname,crs=epsg)
+
+# Can merge, but better to keep separate
+# all_gdf = gpd.GeoDataFrame(gpd.pd.concat([all_gdf,ptsq_df],ignore_index=True))
+# all_gdf.set_crs(epsg, allow_override=True, inplace=True)
+
+# Separate datasets for models with and without domain shps
+
+# shp_dict = {'wdomains':all_gdf,
+#             'woutdomains':ptsq_df,
+#             }
 
 
 def app():
@@ -139,13 +240,32 @@ def app():
         rgroup.add_to(m)
         
     # Add clusters
+    # marker_cluster.add_to(m)
+    
     modelgroup1 = folium.FeatureGroup(name="Model domains").add_to(m)
     modelgroup1.add_child(marker_cluster)
     modelgroup1.add_child(mlayer)
     
+    # for mg,mlayer in zip([modelgroup1,modelgroup2],mlayers):
+    #     mg.add_child(mlayer).add_to(m)
+    # # Add shps to map - looping didn't work for some reason?
+    # modelgroup.add_child(mlayers[0])
+    
+    # modelgroup2 = folium.FeatureGroup(name="Approximate domains")
+    # modelgroup2.add_child(mlayers[1])    
+    
+    # for ishp in mlayers:
+    #     ishp.add_to(map)
+    
+    # modelgroup.add_to(map)
+    # modelgroup1.add_to(m)
+    # modelgroup2.add_to(m)
+    
+    # folium.LayerControl().add_to(m)
     m.add_child(modelgroup1)
+    # m.add_child(modelgroup2)
     m.add_child(folium.LayerControl())
     Fullscreen().add_to(m)
 
     folium_static(m, height=700, width=1400)
-    
+       
