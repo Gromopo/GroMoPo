ec458b7d6107c1e114dccf764e3447367f4a3b3e
Robert Reinecke <33414436+rreinecke@users.noreply.github.com>
2024-03-01 06:50:51 +0100

    Update about.py
    Same fix for the about page after main folder of app was renamed

--- gromopo_app/pages/about.py
+++ gromopo_app/pages/about.py
@@ -7,7 +7,7 @@ def app():
     if platform.system() == 'Windows':
         main_path = Path(".")
     else:
-        main_path = Path("streamlit")
+        main_path = Path("gromopo_app")
         
     markdown = hp.read_markdown_file(str(main_path.joinpath('pages','view','about_page.md')))
     st.markdown(markdown, unsafe_allow_html=True)
